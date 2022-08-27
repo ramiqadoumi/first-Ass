13fb32e38f5939601af7a02320ed34a895b0870b
ramiqadoumi <taharami532@gmail.com>
2022-08-27 17:03:00 +0300

    this is update

--- main.dart
+++ main.dart
@@ -1,3 +1,12 @@
 void main(){
-    String myName="Rami";
+Map ramiInformation{
+    "firstName":"Rami",
+    "lastName":"Ibrahim",
+    "age":22,
+    "educationInfo":{
+        "name":"balq'a applide university",
+        "faculty":"IT",
+        "magor":"SE"
+    }    
+} 
 }
