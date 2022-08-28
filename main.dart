2e249e1cea5cc55a0bb674ca318e393842c55271
ramiqadoumi <taharami532@gmail.com>
2022-08-28 12:09:07 +0300

    this is first ass

--- main.dart
+++ main.dart
@@ -1,12 +1,12 @@
-void main(){
-Map ramiInformation{
-    "firstName":"Rami",
-    "lastName":"Ibrahim",
-    "age":22,
-    "educationInfo":{
-        "name":"balq'a applide university",
-        "faculty":"IT",
-        "magor":"SE"
-    }    
-} 
+void main() {
+  List colors = ["black", "red", "whiate", "pink", "aqua", "apricot"];
+  for (int i = 0; i < colors.length; i++) {
+    if (colors[i].toString().startsWith("a") || colors[i].toString().startsWith("b")) {
+      print(colors[i].toString().toUpperCase());
+    }
+    else{
+      int numChar=colors[i].toString().length;
+      print("sorry this color contains "+numChar.toString()+" and it is not start with a or b");
+    }
+  }
 }
